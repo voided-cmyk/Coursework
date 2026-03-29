3bc12d8a010cf237a8d8f9776ea29226fdea9a6b
voided-cmyk <voided54@gmail.com>
2026-03-29 20:35:24 +0100

    polished some things up

--- Website/javascript.js
+++ Website/javascript.js
@@ -1,1 +1,23 @@
+$(document).ready(function () {
 
+  console.log("Document has been loaded");
+
+
+const greeter = document.getElementById("greeter");
+
+/* add more images later please */
+const images = [
+    "Images/Gym.jpg",
+    "Images/general-img-square.png"
+];
+
+let currentImageIndex = 0;
+
+function changeBackground() {
+    currentImageIndex = (currentImageIndex + 1) % images.length; // Loop through images
+    greeter.style.backgroundImage = `url(${images[currentImageIndex]})`;
+}
+
+setInterval(changeBackground, 5000); // Change background every 5 seconds
+
+});
