c0c29551b0cc44d599757b2a31dd2600b82f9fa6
voided-cmyk <voided54@gmail.com>
2026-03-29 23:37:05 +0100

    Added contact, pages

---
+++ Website/javascript.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    console.log("Document has been loaded");
+
+
+    const greeter = document.getElementById("greeter");
+
+    /* add more images later please */
+    const images = [
+    "Images/Gym.jpg",
+    "Images/general-img-square.png"
+    ];
+
+    let currentImageIndex = 0;
+
+    function changeBackground() {
+    currentImageIndex = (currentImageIndex + 1) % images.length; // image loop
+    greeter.style.backgroundImage = `url(${images[currentImageIndex]})`; // change image background
+    }
+
+    setInterval(changeBackground, 5000); // change every 5 seconds (may change this later)
+
+});
